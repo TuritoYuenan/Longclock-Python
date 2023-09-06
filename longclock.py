5529eb6b19fd293165af45e8ab43033ee27bc577
TuritoYuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-09-06 07:30:27 +0700

    format code

--- longclock.py
+++ longclock.py
@@ -2,40 +2,47 @@ from datetime import datetime, timedelta, timezone
 from dotenv import dotenv_values
 import requests
 
-####################################################
-URL = 'https://api.api-ninjas.com/v1/geocoding?country={}&city={}'
-APIkey = dotenv_values('secrets.env')['APIKEY']
+URL = "https://api.api-ninjas.com/v1/geocoding?country={}&city={}"
+APIkey = dotenv_values("secrets.env")["APIKEY"]
+Header = { "X-Api-Key": APIkey }
+
 ####################################################
 
-def geocode(country: str, city: str) -> dict:
+
+def geocode(country: str, city: str) -> list[object]:
 	"Geocode a location based on the city and country"
 
 	response = requests.get(
-		url = URL.format(country, city),
-		headers = {'X-Api-Key': APIkey}
+		url=URL.format(country, city), headers=Header # type: ignore
 	)
 
 	if response.status_code == requests.codes.ok:
-		output = response.json()[0]
+		output = response.json()
 
 		print(output)
 		return output
 
 	else:
 		print("Error:", response.status_code, response.text)
+		raise requests.HTTPError
+
 
 ####################################################
-UTCtime = datetime.now(timezone.utc)
-debugTimeFormat: str = '%Y-%m-%d %H:%M:%S'
-####################################################
+
 
 def clock(longitude: float) -> datetime:
 	"Return time based on longitude"
 
-	offset: int = timedelta(hours = round(longitude / 15))
+	offset: timedelta = timedelta(hours=round(longitude / 15))
 	longtime = datetime.now(timezone(offset))
 
 	return longtime
 
+
+####################################################
+
+UTCtime = datetime.now(timezone.utc)
+debugTimeFormat: str = "%Y-%m-%d %H:%M:%S"
+
 print(UTCtime.strftime(debugTimeFormat))
-print(clock(108).strftime(debugTimeFormat))
+print(clock(0).strftime(debugTimeFormat))
--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-from interactions import *
+#!/bin/python
+#
+# Discord bot for Longclock
+
+from interactions import (
+	Client,
+	Embed, EmbedAuthor, EmbedFooter,
+	OptionType, SlashContext,
+	slash_command, slash_option, listen
+)
+
 from dotenv import dotenv_values
 import longclock
 
-env = dotenv_values('secrets.env')
+env: dict[str, str | None] = dotenv_values("secrets.env")
 
-robot = Client(
-	token = env['TOKEN'],
-	debug_scope = env['SERVER']
+robot: Client = Client(token=env["TOKEN"], debug_scope=env["SERVER"])
+
+timeFmt: str = "%X"
+"Time format"
+dateFmt: str = "%d of %B, %Y"
+"Date format"
+
+
+@slash_command(name="longtime", description="Current Longtime of a location")
+@slash_option(
+	name="city", description="City", opt_type=OptionType.STRING, required=True
 )
-
-timeFmt: str = '%X'
-dateFmt: str = '%d of %B, %Y'
-
-@slash_command(name='longtime', description='Current Longtime of a location')
-@slash_option(name='city', description='City', opt_type=OptionType.STRING, required=True)
-@slash_option(name='country', description='Country the city is in', opt_type=OptionType.STRING, required=True)
+@slash_option(
+	name="country",
+	description="Country the city is in",
+	opt_type=OptionType.STRING,
+	required=True,
+)
 async def longtime(ctx: SlashContext, city: str, country: str):
-    loc = longclock.geocode(country, city)
-    ltime = longclock.clock(loc['longitude'])
+	loc = longclock.geocode(country, city)[0]
+	ltime = longclock.clock(loc["longitude"])
 
-    message = Embed(
-        title = f'{ltime.strftime(timeFmt)} | {ltime.strftime(dateFmt)}',
-        description = f'Longtime of {loc["name"]}, {loc["country"]}',
-        author = EmbedAuthor('Longclock Robot'),
-        footer = EmbedFooter('2023 Turito Yuenan | Non-commercial'),
-        color = 0xFF0000
+	message = Embed(
+		title=f"{ltime.strftime(timeFmt)} | {ltime.strftime(dateFmt)}",
+		description=f'Longtime of {loc["name"]}, {loc["country"]}',
+		author=EmbedAuthor("Longclock Robot"),
+		footer=EmbedFooter("2023 Turito Yuenan | Non-commercial"),
+		color=0xFF0000,
 	)
 
-    await ctx.send(embeds=message)
+	await ctx.send(embeds=message)
+
 
 @listen()
-async def on_ready(): print('Ready! Serving Discord server', env['SERVER'])
-"Announce when ready"
+async def on_ready():
+	"Announce when ready"
+	print("Ready! Serving Discord server", env["SERVER"])
+
 
 robot.start()
-"Activate the robot"
