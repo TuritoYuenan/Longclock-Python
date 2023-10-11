e09438e5ac503e65a928a26f2d0f3f72591ff97d
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-11 17:09:04 +0700

    add navbar

--- app/src/main/java/vn/turito/longitime/MainActivity.kt
+++ app/src/main/java/vn/turito/longitime/MainActivity.kt
@@ -1,43 +1,72 @@
 package vn.turito.longitime
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Place
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import vn.turito.longitime.ui.theme.LongitimeTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            LongitimeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent { AppRoot() }
+	}
+}
+
+val myIcons = Icons.Rounded
+
+data class NavbarItem(var name: String, var icon: ImageVector)
+
+val navbarItems = listOf(
+	NavbarItem("Clock", myIcons.Home),
+	NavbarItem("Saved Clocks", myIcons.Favorite),
+	NavbarItem("Location", myIcons.Place),
+	NavbarItem("Settings", myIcons.Settings),
+)
+
+@Preview(
+	name = "App Root",
+	uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun AppRoot() {
+	LongitimeTheme {
+		Scaffold(
+			bottomBar = { Navbar() },
+			modifier = Modifier.padding(1.dp)
+		) { paddingValues ->
+			Column(Modifier.padding(paddingValues)) {}
+		}
+	}
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LongitimeTheme {
-        Greeting("Android")
-    }
+fun Navbar() {
+	NavigationBar {
+		navbarItems.forEachIndexed { _, item ->
+			NavigationBarItem(
+				icon = { Icon(item.icon, item.name) },
+				label = { Text(item.name) },
+				selected = false,
+				onClick = { /*TODO*/ }
+			)
+		}
+	}
 }
