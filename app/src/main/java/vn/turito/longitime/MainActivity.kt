c38449fb64b2a3fafa31162cd351582683a7cff9
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-13 08:20:48 +0700

    extract all navbar codes to a component add a sample clock for styling

--- app/src/main/java/vn/turito/longitime/MainActivity.kt
+++ app/src/main/java/vn/turito/longitime/MainActivity.kt
@@ -1,15 +1,21 @@
 package vn.turito.longitime
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import vn.turito.longitime.ui.components.NavBar
 import vn.turito.longitime.ui.theme.LongitimeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,30 +25,24 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
-@Preview(name = "Root")
+@Preview(
+	name = "Dark mode",
+	uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
 @Composable
 fun AppRoot() {
 	LongitimeTheme {
 		Scaffold(
-			bottomBar = { Navbar() },
-			modifier = Modifier.padding(1.dp)
+			bottomBar = { NavBar() }
 		) { paddingValues ->
-			Column(Modifier.padding(paddingValues)) {}
-		}
-	}
-}
-
-@Composable
-fun Navbar() {
-	var active by remember { mutableIntStateOf(0) }
-	NavigationBar {
-		navbarItems.forEachIndexed { index, item ->
-			NavigationBarItem(
-				icon = { Icon(item.icon, item.name) },
-				label = { Text(item.name) },
-				onClick = { active = index },
-				selected = index == active,
-			)
+			Column(Modifier.padding(paddingValues)) {
+				Text(
+					"12:45",
+					Modifier.width(IntrinsicSize.Max),
+					textAlign = TextAlign.Center,
+					style = MaterialTheme.typography.displayLarge
+				)
+			}
 		}
 	}
 }
--- app/src/main/java/vn/turito/longitime/ui/components/NavigationBar.kt
+++ app/src/main/java/vn/turito/longitime/ui/components/NavigationBar.kt
@@ -1,10 +1,15 @@
-package vn.turito.longitime
+package vn.turito.longitime.ui.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Place
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class NavbarItem(var name: String, var icon: ImageVector)
@@ -15,3 +20,18 @@ val navbarItems = listOf(
 	NavbarItem("Location", Icons.Rounded.Place),
 	NavbarItem("Settings", Icons.Rounded.Settings),
 )
+
+@Composable
+fun NavBar() {
+	var active by remember { mutableIntStateOf(0) }
+	NavigationBar {
+		navbarItems.forEachIndexed { index, item ->
+			NavigationBarItem(
+				icon = { Icon(item.icon, item.name) },
+				label = { Text(item.name) },
+				onClick = { active = index },
+				selected = index == active,
+			)
+		}
+	}
+}
