192c904d6611ba1037d0aeecf87e9a434dd2429d
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-17 11:10:50 +0700

    Temporarily add Navigation component

--- app/src/main/java/vn/turito/longitime/theme/Theme.kt
+++ app/src/main/java/vn/turito/longitime/theme/Theme.kt
@@ -85,29 +85,20 @@ fun LTimeTheme(
 	val colorScheme = when {
 		dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
 			val context = LocalContext.current
-			if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(
-				context
-			)
+			if (darkTheme) dynamicDarkColorScheme(context)
+			else dynamicLightColorScheme(context)
 		}
 
 		darkTheme -> lunarpunk
 		else -> solarpunk
 	}
 	val view = LocalView.current
-	if (!view.isInEditMode) {
-		SideEffect {
-			val window = (view.context as Activity).window
-			window.statusBarColor = colorScheme.primary.toArgb()
-			WindowCompat.getInsetsController(
-				window,
-				view
-			).isAppearanceLightStatusBars = darkTheme
-		}
+	if (!view.isInEditMode) SideEffect {
+		val window = (view.context as Activity).window
+		window.statusBarColor = colorScheme.primary.toArgb()
+		WindowCompat.getInsetsController(window, view)
+			.isAppearanceLightStatusBars = darkTheme
 	}
 
-	MaterialTheme(
-		content = content,
-		colorScheme = colorScheme,
-		typography = Typography,
-	)
+	MaterialTheme(content = content, colorScheme = colorScheme)
 }
