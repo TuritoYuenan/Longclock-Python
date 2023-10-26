dd0e4188d7f9e63e485e8a29f5f41f60a44e1abb
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-26 08:11:05 +0700

    Migrate to version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,22 +62,26 @@ android {
 }
 
 dependencies {
-	implementation("androidx.activity:activity-compose:1.8.0")
-	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.compose.material3:material3:1.1.2")
-	implementation("androidx.compose.ui:ui:1.5.3")
-	implementation("androidx.compose.ui:ui-graphics:1.5.3")
-	implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-	implementation("androidx.navigation:navigation-compose:2.7.4")
-	implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-	implementation(platform("androidx.compose:compose-bom:2023.10.00"))
-	androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.3")
-	androidTestImplementation("androidx.test.ext:junit:1.1.5")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
-	debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
-	debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
-	testImplementation("junit:junit:4.13.2")
+	val composeBom = platform(libs.androidx.compose.bom)
+
+	implementation(composeBom)
+	implementation(libs.androidx.compose.material3)
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.ui.graphics)
+	implementation(libs.androidx.compose.ui.tooling.preview)
+	implementation(libs.androidx.activity.compose)
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.lifecycle.runtime.ktx)
+	implementation(libs.androidx.navigation.compose)
+
+	// Android Test
+	androidTestImplementation(composeBom)
+	androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
+	debugImplementation(libs.androidx.compose.ui.tooling)
+	debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+	// Unit Test
+	testImplementation(libs.junit)
 }
