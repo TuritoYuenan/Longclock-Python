94d03121428060d9a356aa885dd1ebf1e7f6a85c
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-29 07:57:18 +0700

    move kotlin code to kotlin directory

--- app/src/main/kotlin/vn/turito/longitime/ClockView.kt
+++ app/src/main/kotlin/vn/turito/longitime/ClockView.kt
@@ -1,11 +1,9 @@
 package vn.turito.longitime
 
 import android.os.Build
+import android.widget.TextClock
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,9 +11,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
+@Preview
+@Composable
+fun CustomView() {
+	AndroidView(
+		modifier = Modifier.fillMaxSize(), // Occupy the max size in the Compose UI tree
+		factory = { context -> TextClock(context) }
+	)
+}
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
